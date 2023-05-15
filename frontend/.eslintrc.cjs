df5a35071b753b63950f3ab5f5168909f07883b0
Non-J <works@jirawut.com>
2023-05-15 10:50:50 +0900

    feat(ci): enable typed linting

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,9 +1,14 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
   extends: [
     "eslint:recommended",
     "semistandard",
     "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:react/recommended",
     "plugin:prettier/recommended",
   ],
