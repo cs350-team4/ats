b80328a6e57ec47d2ac76ce393dfd90acae6006f
Aziz Huseynov <aziz2004huseynov@gmail.com>
2023-05-18 20:08:36 +0900

    feat(backend): Add auth endpoints
    Endpoints:
    - `/auth/generateToken`
    - `/auth/publicKey`
    This commit also handles connecting to client db.
    
    Co-authored-by: Murad Bashirov <muradb@kaist.ac.kr>

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,26 +1,43 @@
 from collections.abc import Generator
+from datetime import datetime
 from typing import List
 
+import bcrypt
+import jwt
 from fastapi import Depends, FastAPI, HTTPException
-from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel import Session, create_engine, select
 
 from api import crud
-from api.models import GameCreate, GameRead, GameUpdate
+from api.models import (
+    Client,
+    GameCreate,
+    GameRead,
+    GameUpdate,
+    GenerateToken,
+    OwnedModel,
+)
 from api.settings import settings
 
 assert settings.DATABASE_URI is not None
 # pool_pre_ping -> test conn is viable at the start of each conn
 engine = create_engine(settings.DATABASE_URI, echo=True, pool_pre_ping=True)
+# TODO: make type checker pass
+client_engine = create_engine(settings.CLIENT_DB_URI, echo=True, pool_pre_ping=True)  # type: ignore
 
 
 def create_db_and_tables() -> None:
     # TODO: Don't create Client
-    SQLModel.metadata.create_all(engine)
+    OwnedModel.metadata.create_all(engine)
 
 
 # TODO: get client session
 def get_session() -> Generator[Session, None, None]:
     with Session(engine) as session:  # type: ignore
+        yield session
+
+
+def get_client_session() -> Generator[Session, None, None]:
+    with Session(client_engine) as session:  # type: ignore
         yield session
 
 
@@ -64,5 +81,29 @@ def update_game(
 def delete_game(*, session: Session = Depends(get_session), game_id: int):
     deleted_game = crud.delete_game(session, game_id)
     if not deleted_game:
-        raise HTTPException(status_code=404, detail="Hero not found")
+        raise HTTPException(status_code=404, detail="Game not found")
     return deleted_game
+
+
+@app.post("/auth/generateToken")
+def generate_token(
+    *, session: Session = Depends(get_client_session), payload: GenerateToken
+):
+    stmt = select(Client).where(Client.username == payload.username)
+    result = session.exec(stmt).first()
+    if result is None:
+        raise HTTPException(status_code=403, detail="Authentication failed")
+    if bcrypt.checkpw(payload.password.encode("utf8"), result.password.encode("utf8")):
+        jwToken = jwt.encode(
+            {"name": result.username, "iat": datetime.now()},
+            settings.PRIVATE_KEY,
+            algorithm="ES256",
+        )
+        return {"auth_token": jwToken}
+    else:
+        raise HTTPException(status_code=403, detail="Authentication failed")
+
+
+@app.get("/auth/publicKey")
+def public_key():
+    return {"publicKey": settings.PUBLIC_KEY}
