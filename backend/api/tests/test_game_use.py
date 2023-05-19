ae7379a538798ac3d4302fedc4e33006160d009a
Aziz Huseynov <aziz2004huseynov@gmail.com>
2023-05-20 06:40:41 +0900

    chore(test): Add client db for local testing

--- backend/api/tests/test_game_use.py
+++ backend/api/tests/test_game_use.py
@@ -1,24 +1,8 @@
-from fastapi.testclient import TestClient
-from sqlmodel import create_engine
-
-from ..db.engine import local_engine
-from ..dependencies import GetSession
-from ..main import app
-from ..models import OwnedModel
 from ..settings import settings
 from .utils.auth import generate_manager_token, generate_token
 from .utils.game import create_random_game
 from .utils.game_machine import end_game, reset_game, start_game
-
-assert settings.TEST_DB_URI is not None
-
-test_engine = create_engine(settings.TEST_DB_URI)
-
-OwnedModel.metadata.create_all(test_engine)
-
-app.dependency_overrides[GetSession(local_engine)] = GetSession(test_engine)
-
-client = TestClient(app)
+from .utils.mock import client
 
 manager_tok = generate_manager_token(settings.PRIVATE_KEY, "man1")
 client_tok = generate_token(client)
---
+++ backend/api/tests/utils/mock.py
@@ -0,0 +1,22 @@
+from fastapi.testclient import TestClient
+from sqlmodel import create_engine
+
+from ...db.engine import client_engine, local_engine
+from ...dependencies import GetSession
+from ...main import app
+from ...models import ClientModel, OwnedModel
+from ...settings import settings
+
+assert settings.TEST_DB_URI is not None
+assert settings.TEST_CLIENT_DB_URI is not None
+
+test_engine = create_engine(settings.TEST_DB_URI)
+test_client_engine = create_engine(settings.TEST_CLIENT_DB_URI)
+
+OwnedModel.metadata.create_all(test_engine)
+ClientModel.metadata.create_all(test_client_engine)
+
+app.dependency_overrides[GetSession(local_engine)] = GetSession(test_engine)
+app.dependency_overrides[GetSession(client_engine)] = GetSession(test_client_engine)
+
+client = TestClient(app)
