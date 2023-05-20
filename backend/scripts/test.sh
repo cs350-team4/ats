1b5956a932b5f40573f94ecdcf1e321f869aa7cd
Aziz Huseynov <aziz2004huseynov@gmail.com>
2023-05-20 18:52:02 +0900

    chore(CI): fix dotenv bug

--- backend/scripts/test.sh
+++ backend/scripts/test.sh
@@ -3,26 +3,28 @@
 set -e
 set -x
 
-set -o allexport
-source .env
-set +o allexport
+if [[ -z "$CI" ]]; then
+    set -o allexport
+    source .env
+    set +o allexport
 
-# Disable pager output from postgre
-export PAGER=
+    # Disable pager output from postgre
+    export PAGER=
 
-export PGHOST=$POSTGRES_SERVER
-export PGDATABASE=$POSTGRES_TEST_DB
-export PGUSER=$POSTGRES_USER
-export PGPASSWORD=$POSTGRES_PASSWORD
+    export PGHOST=$POSTGRES_SERVER
+    export PGDATABASE=$POSTGRES_TEST_DB
+    export PGUSER=$POSTGRES_USER
+    export PGPASSWORD=$POSTGRES_PASSWORD
 
-psql -f data/ats.sql
+    psql -f data/ats.sql
 
-export PGHOST=$POSTGRES_CLIENT_SERVER
-export PGDATABASE=$POSTGRES_TEST_CLIENT_DB
-export PGUSER=$POSTGRES_CLIENT_USER
-export PGPASSWORD=$POSTGRES_CLIENT_PASSWORD
+    export PGHOST=$POSTGRES_CLIENT_SERVER
+    export PGDATABASE=$POSTGRES_TEST_CLIENT_DB
+    export PGUSER=$POSTGRES_CLIENT_USER
+    export PGPASSWORD=$POSTGRES_CLIENT_PASSWORD
 
-psql -f data/client.sql
-psql -c "SELECT * FROM client"
+    psql -f data/client.sql
+    psql -c "SELECT * FROM client"
+fi
 
 pytest --cov=api --cov-report=term-missing api/tests "${@}"
