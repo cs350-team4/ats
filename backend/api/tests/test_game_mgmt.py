db84aff9b3599cdc43540619e91810bf39fb635e
Aziz Huseynov <aziz2004huseynov@gmail.com>
2023-05-23 01:05:21 +0900

    test(backend): Add more tests for game management

--- backend/api/tests/test_game_mgmt.py
+++ backend/api/tests/test_game_mgmt.py
@@ -3,11 +3,16 @@ import pytest
 from ..settings import settings
 from .utils.auth import generate_manager_token
 from .utils.game import (
+    create_game_invalid,
+    create_game_valid,
     create_random_game,
-    delete_game,
-    get_all_games,
-    get_one_game,
-    update_game,
+    delete_game_valid,
+    get_all_games_valid,
+    get_one_game_invalid,
+    get_one_game_valid,
+    update_game_invalid,
+    update_game_random,
+    update_game_valid,
 )
 from .utils.mock import client
 
@@ -24,7 +29,7 @@ def test_create_get_game():
 
     game_id = game.id
 
-    game2 = get_one_game(client, game_id, manager_tok)
+    game2 = get_one_game_valid(client, manager_tok, game_id)
     assert game2 == game
     # assert game2.name == game.name
     # assert game2.exchange_rate == game.exchange_rate
@@ -33,12 +38,75 @@ def test_create_get_game():
 
 def test_update_delete_list_game():
     game = create_random_game(client, manager_tok)
-    new_game = update_game(client, game.id, manager_tok)
+    new_game = update_game_random(client, manager_tok, game.id)
     assert new_game.name != game.name
     assert new_game.exchange_rate != game.exchange_rate
     assert new_game.exchange_rate != game.exchange_rate
 
-    deleted_game = delete_game(client, game.id, manager_tok)
+    deleted_game = delete_game_valid(client, manager_tok, game.id)
     assert deleted_game == new_game
-    games = get_all_games(client, manager_tok)
+    games = get_all_games_valid(client, manager_tok)
     assert deleted_game not in games
+
+
+def test_create_game_invalid():
+    assert 422 == create_game_invalid(client, manager_tok, "name", "test", 1)
+    assert 422 == create_game_invalid(client, manager_tok, "name", "a" * 32, -1)
+    assert 422 == create_game_invalid(client, manager_tok, "nam" * 100, "a" * 32, 1)
+    assert 403 == create_game_invalid(client, "Invalid", "name", "a" * 32, 1)
+
+
+def test_update_game_single_field():
+    game = create_random_game(client, manager_tok)
+    new_name = game.name[::-1]
+    updated_game = update_game_valid(client, manager_tok, game.id, new_name)
+
+    assert updated_game.name == new_name
+    assert updated_game.exchange_rate == game.exchange_rate
+    assert updated_game.password == game.password
+
+
+def test_get_invalid_game():
+    invalid_id = 9999
+    assert 404 == get_one_game_invalid(client, manager_tok, invalid_id)
+
+
+def test_update_invalid_game():
+    invalid_id = 9999
+    assert 404 == update_game_invalid(client, manager_tok, invalid_id, "New name")
+
+
+def test_delete_invalid_game():
+    invalid_id = 9999
+    response = client.delete(
+        f"/games/{invalid_id}", headers={"Authorization": f"Bearer {manager_tok}"}
+    )
+    assert response.status_code == 404
+
+
+def test_create_game_no_auth():
+    assert 403 == create_game_invalid(client, "", "name", "p" * 32, 1)
+
+
+def test_update_game_no_auth():
+    game = create_random_game(client, manager_tok)
+    assert 403 == update_game_invalid(client, "", game.id, "New name")
+
+
+def test_get_game_no_auth():
+    game = create_random_game(client, manager_tok)
+    response = client.get(f"/games/{game.id}", headers={"Authorization": ""})
+    assert response.status_code == 403
+
+
+def test_delete_game_no_auth():
+    game = create_random_game(client, manager_tok)
+    response = client.delete(f"/games/{game.id}", headers={"Authorization": ""})
+    assert response.status_code == 403
+
+
+@pytest.mark.skip(reason="Not a issue according to SRS")
+def test_create_game_duplicate_name():
+    game_name = "new_game_name_space_game"
+    create_game_valid(client, manager_tok, game_name, "p" * 32, 1)
+    assert 422 == create_game_invalid(client, manager_tok, game_name, "p" * 32, 2)
--- backend/api/tests/utils/game.py
+++ backend/api/tests/utils/game.py
@@ -1,19 +1,53 @@
+from typing import Optional
+
 from fastapi.testclient import TestClient
+from httpx import Response
 
 from api.models import GameRead
 
 from . import random_32b_string, random_float
 
 
-def create_random_game(client: TestClient, manager_tok: str) -> GameRead:
-    name = random_32b_string()
-    pwd = random_32b_string()
-    ex_rate = random_float()
-    response = client.post(
+def create_game(
+    client: TestClient, manager_tok: str, name: str, pwd: str, ex_rate: float
+) -> Response:
+    return client.post(
         "/games/",
         json={"name": name, "exchange_rate": ex_rate, "password": pwd},
         headers={"Authorization": f"Bearer {manager_tok}"},
     )
+
+
+def update_game(
+    client: TestClient,
+    manager_tok: str,
+    game_id: int,
+    new_name: str,
+    new_pwd: Optional[str] = None,
+    new_ex_rate: Optional[float] = None,
+) -> Response:
+    payload = {"name": new_name, "password": new_pwd, "exchange_rate": new_ex_rate}
+    if not new_pwd:
+        payload.pop("password")
+    if not new_ex_rate:
+        payload.pop("exchange_rate")
+    return client.patch(
+        f"/games/{game_id}",
+        json=payload,
+        headers={"Authorization": f"Bearer {manager_tok}"},
+    )
+
+
+def get_one_game(client: TestClient, manager_tok: str, game_id: int) -> Response:
+    return client.get(
+        f"/games/{game_id}", headers={"Authorization": f"Bearer {manager_tok}"}
+    )
+
+
+def create_game_valid(
+    client: TestClient, manager_tok: str, name: str, pwd: str, ex_rate: float
+) -> GameRead:
+    response = create_game(client, manager_tok, name, pwd, ex_rate)
     assert response.status_code == 200, response.text
 
     data = response.json()
@@ -25,37 +59,62 @@ def create_random_game(client: TestClient, manager_tok: str) -> GameRead:
     return GameRead.parse_obj(data)
 
 
-def update_game(client: TestClient, game_id: int, manager_tok: str) -> GameRead:
-    new_pwd = random_32b_string()
-    new_name = random_32b_string()
-    new_ex_rate = random_float()
-    response = client.patch(
-        f"/games/{game_id}",
-        json={"name": new_name, "exchange_rate": new_ex_rate, "password": new_pwd},
-        headers={"Authorization": f"Bearer {manager_tok}"},
-    )
+def create_game_invalid(
+    client: TestClient, manager_tok: str, name: str, pwd: str, ex_rate: float
+) -> int:
+    response = create_game(client, manager_tok, name, pwd, ex_rate)
+    return response.status_code
+
+
+def update_game_valid(
+    client: TestClient,
+    manager_tok: str,
+    game_id: int,
+    new_name: str,
+    new_pwd: Optional[str] = None,
+    new_ex_rate: Optional[float] = None,
+) -> GameRead:
+    response = update_game(client, manager_tok, game_id, new_name, new_pwd, new_ex_rate)
     assert response.status_code == 200, response.text
 
     data = response.json()
-    assert data["name"] == new_name
-    assert data["exchange_rate"] == new_ex_rate
-    assert data["password"] == new_pwd
+    if new_name:
+        assert data["name"] == new_name
+    if new_ex_rate:
+        assert data["exchange_rate"] == new_ex_rate
+    if new_pwd:
+        assert data["password"] == new_pwd
     assert data["id"] == game_id
 
     return GameRead.parse_obj(data)
 
 
-def get_one_game(client: TestClient, game_id: int, manager_tok: str) -> GameRead:
-    response = client.get(
-        f"/games/{game_id}", headers={"Authorization": f"Bearer {manager_tok}"}
-    )
+def update_game_invalid(
+    client: TestClient,
+    manager_tok: str,
+    game_id: int,
+    new_name: str,
+    new_pwd: Optional[str] = None,
+    new_ex_rate: Optional[float] = None,
+) -> int:
+    response = update_game(client, manager_tok, game_id, new_name, new_pwd, new_ex_rate)
+    return response.status_code
+
+
+def get_one_game_valid(client: TestClient, manager_tok: str, game_id: int) -> GameRead:
+    response = get_one_game(client, manager_tok, game_id)
     assert response.status_code == 200, response.text
     data = response.json()
 
     return GameRead.parse_obj(data)
 
 
-def delete_game(client: TestClient, game_id: int, manager_tok: str) -> GameRead:
+def get_one_game_invalid(client: TestClient, manager_tok: str, game_id: int) -> int:
+    response = get_one_game(client, manager_tok, game_id)
+    return response.status_code
+
+
+def delete_game_valid(client: TestClient, manager_tok: str, game_id: int) -> GameRead:
     response = client.delete(
         f"/games/{game_id}", headers={"Authorization": f"Bearer {manager_tok}"}
     )
@@ -65,9 +124,26 @@ def delete_game(client: TestClient, game_id: int, manager_tok: str) -> GameRead:
     return GameRead.parse_obj(data)
 
 
-def get_all_games(client: TestClient, manager_tok: str) -> list[GameRead]:
+def get_all_games_valid(client: TestClient, manager_tok: str) -> list[GameRead]:
     response = client.get("/games/", headers={"Authorization": f"Bearer {manager_tok}"})
     assert response.status_code == 200, response.text
     data = response.json()
 
     return [GameRead.parse_obj(x) for x in data]
+
+
+def create_random_game(client: TestClient, manager_tok: str) -> GameRead:
+    name = random_32b_string()
+    pwd = random_32b_string()
+    ex_rate = random_float()
+
+    return create_game_valid(client, manager_tok, name, pwd, ex_rate)
+
+
+def update_game_random(client: TestClient, manager_tok: str, game_id: int) -> GameRead:
+    new_pwd = random_32b_string()
+    new_name = random_32b_string()
+    new_ex_rate = random_float()
+    return update_game_valid(
+        client, manager_tok, game_id, new_name, new_pwd, new_ex_rate
+    )
