c20fd87fcb1bda0e078b4d3dc6b70c9ebb7e044e
Aziz Huseynov <aziz2004huseynov@gmail.com>
2023-05-23 01:04:07 +0900

    test(backend): Add more tests for coupon checksum

--- backend/api/tests/test_serial_checksum.py
+++ backend/api/tests/test_serial_checksum.py
@@ -1,6 +1,8 @@
+import random
+
 from api.utils import generate_serial, verify_serial
 
-SHUFFLE = ["9", "3", "5", "4", "8", "7", "1", "6", "2", "0"]
+SHUFFLE = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 
 def unit_serial_checksum_flip(i=9):
@@ -8,9 +10,25 @@ def unit_serial_checksum_flip(i=9):
     assert verify_serial(serial_num)
 
     serial_num = serial_num[:i] + SHUFFLE[int(serial_num[i])] + serial_num[i + 1 :]
-    assert not verify_serial(serial_num)
+    assert serial_num[i] == SHUFFLE[int(serial_num[i])] or not verify_serial(serial_num)
 
 
-def test_serial_checksum():
-    for i in range(10):
-        unit_serial_checksum_flip(i)
+def unit_serial_checksum_swap(i=0):
+    serial_num = generate_serial()
+    assert verify_serial(serial_num)
+
+    serial_num = (
+        serial_num[:i] + serial_num[i + 1] + serial_num[i] + serial_num[i + 2 :]
+    )
+    assert serial_num[i] == serial_num[i + 1] or not verify_serial(serial_num)
+
+
+def test_serial_checksum_flip():
+    random.shuffle(SHUFFLE)
+    for i in range(1000):
+        unit_serial_checksum_flip(i % 10)
+
+
+def test_serial_checksum_swap():
+    for i in range(1000):
+        unit_serial_checksum_swap(i % 9)
