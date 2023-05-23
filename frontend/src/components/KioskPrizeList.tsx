ffe579074fc7f0d3f43ad2ad5a61447d0dd3f737
Non-J <works@jirawut.com>
2023-05-23 16:41:29 +0900

    feat(ui): add pagination to prize list

--- frontend/src/components/KioskPrizeList.tsx
+++ frontend/src/components/KioskPrizeList.tsx
@@ -5,14 +5,16 @@ import {
   Group,
   Chip,
   NativeSelect,
-  Center,
   Box,
   useMantineTheme,
+  Pagination,
+  Flex,
 } from "@mantine/core";
 import KioskPrizeDisplay from "./KioskPrizeDisplay";
 import type { Prize } from "../data/prize";
 import { useDisclosure } from "@mantine/hooks";
 import { useState, useMemo } from "react";
+import { PRIZE_LIST_PAGE_LENGTH } from "../global.config";
 
 export interface KioskPrizeListProps {
   /**
@@ -55,42 +57,57 @@ const KioskPrizeList: React.FC<KioskPrizeListProps> = ({
     useDisclosure(false);
   const [sortOrder, setSortOrder] = useState<SortOrderT>(SortOrder[0]);
 
-  const displayPrizes = useMemo(() => {
-    let displayPrizes = prizes;
+  const filteredPrizes = useMemo(() => {
+    let filteredPrizes = prizes;
 
     // filters
     if (filterInStock) {
-      displayPrizes = displayPrizes.filter((item) => item.stock > 0);
+      filteredPrizes = filteredPrizes.filter((item) => item.stock > 0);
     }
     if (filterCanAfford) {
-      displayPrizes = displayPrizes.filter(
-        (item) => currentTickets > item.cost
+      filteredPrizes = filteredPrizes.filter(
+        (item) => currentTickets > item.price
       );
     }
 
     // sorts
-    displayPrizes.sort((lhs, rhs) => {
-      switch (sortOrder) {
-        case "Name (A-Z)":
-          return lhs.name.localeCompare(rhs.name);
-        case "Name (Z-A)":
-          return rhs.name.localeCompare(lhs.name);
-        case "Price (Low-High)":
-          return lhs.cost === rhs.cost ? 0 : lhs.cost < rhs.cost ? -1 : 1;
-        case "Price (High-Low)":
-          return rhs.cost === lhs.cost ? 0 : rhs.cost < lhs.cost ? -1 : 1;
-        default:
-          return 0;
-      }
-    });
-    return displayPrizes;
+    switch (sortOrder) {
+      case "Name (A-Z)":
+        filteredPrizes.sort((lhs, rhs) => lhs.name.localeCompare(rhs.name));
+        break;
+      case "Name (Z-A)":
+        filteredPrizes.sort((lhs, rhs) => rhs.name.localeCompare(lhs.name));
+        break;
+      case "Price (Low-High)":
+        filteredPrizes.sort((lhs, rhs) => lhs.price - rhs.price);
+        break;
+      case "Price (High-Low)":
+        filteredPrizes.sort((lhs, rhs) => rhs.price - lhs.price);
+        break;
+    }
+
+    return filteredPrizes;
   }, [sortOrder, filterInStock, filterCanAfford, prizes, currentTickets]);
+
+  // pagination
+  const [activePage, setPage] = useState(1);
+  const pageCount = Math.ceil(filteredPrizes.length / PRIZE_LIST_PAGE_LENGTH);
+  const displayPrizes = filteredPrizes.slice(
+    (activePage - 1) * PRIZE_LIST_PAGE_LENGTH,
+    activePage * PRIZE_LIST_PAGE_LENGTH
+  );
 
   return (
     <>
-      <Container size="lg">
+      <Container fluid>
         {/* Item filters */}
-        <Center mb="md">
+        <Flex
+          m="md"
+          gap="md"
+          direction="column"
+          align="center"
+          justify="center"
+        >
           <Group>
             <NativeSelect
               data={SortOrderV}
@@ -106,7 +123,9 @@ const KioskPrizeList: React.FC<KioskPrizeListProps> = ({
               In Stock Only
             </Chip>
           </Group>
-        </Center>
+
+          <Pagination total={pageCount} value={activePage} onChange={setPage} />
+        </Flex>
 
         {displayPrizes.length === 0 ? (
           // List is empty
@@ -123,7 +142,7 @@ const KioskPrizeList: React.FC<KioskPrizeListProps> = ({
             }}
           >
             {displayPrizes.map((prize) => (
-              <Box key={prize.prizeId}>
+              <Box key={prize.id}>
                 <KioskPrizeDisplay
                   prize={prize}
                   currentTickets={currentTickets}
--- frontend/src/global.config.ts
+++ frontend/src/global.config.ts
@@ -2,3 +2,7 @@
 export const API_ROOT = "http://localhost:8080";
 // The time public key will be cached
 export const PUBLIC_KEY_CACHE_TIME = 15 * 60 * 1000; // unit: ms
+// Maximum number of prizes to pull from server
+export const PRIZE_LIST_LIMIT = 1000000;
+// Number of prizes per page
+export const PRIZE_LIST_PAGE_LENGTH = 24;
