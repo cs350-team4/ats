27da6fbbfcad3acfcb436dd0c9d19a48b2524e13
SirCollypso <aminkapkan444@gmail.com>
2023-06-18 21:30:08 +0900

    refactor(app): prepare register script for content policy

---
+++ pc-app/src/js/register.js
@@ -0,0 +1,33 @@
+/*global ipcRenderer*/
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('register-form').addEventListener('submit', (event) => { // eslint-disable-line no-unused-vars
+    event.preventDefault();
+    const jwt = document.getElementById("jwt").value;
+    const password = document.getElementById("password").value;
+    ipcRenderer.send('register:submit', {jwt, password});
+  });
+});
+
+ipcRenderer.on('register:success', (_options) => {
+  window.location.href = `userlist.html`;
+  console.log("success");
+});
+
+ipcRenderer.on('register:failure', (options) => {
+  sendError(`Failure: ${options}`);
+});
+
+const sendError = (error) => {
+  const errorMessage = document.getElementById("error-message");
+  errorMessage.textContent = error;
+}
+
+const nullError = () => {
+  const errorMessage = document.getElementById("error-message");
+  errorMessage.textContent = null;
+}
+
+document.querySelector('.back-button').addEventListener('click', (_event) => { // eslint-disable-line no-unused-vars
+  window.location.href = `userlist.html`;
+});
